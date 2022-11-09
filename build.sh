b5fb02052a8a1684ef361b7462e3e2ab5336622a
Pierre Seznec <ps@massa.net>
2022-11-09 18:02:44 +0100

    update simulator downloader script

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -ex
 PLUGIN=playground-plugin
 
 mkdir -p "build/$PLUGIN/simulator"
-# node scripts/get-simulator.js -o build/$PLUGIN
 cp simulator/massa-sc-tester* build/$PLUGIN/simulator
 go build -o build/$PLUGIN/thyra-playground-plugin cmd/main.go
 cp manifest.json build/$PLUGIN/
--- scripts/get-simulator.js
+++ scripts/get-simulator.js
@@ -7,19 +7,27 @@ const TESTER_RELEASE_URL =
   "https://github.com/massalabs/massa-sc-tester/releases/download";
 const VERSION = "v0.3.1";
 
-console.log(`Downloading simulator binary for ${process.platform}`);
+// env var used by the CI
+const GOOS = process.env["GOOS"];
+const GOARCH = process.env["GOARCH"];
+
 let fileName;
-switch (process.platform) {
-  case "darwin":
-    fileName = "release_macos.tar.gz";
-    break;
-  case "linux":
-    fileName = "release_linux.tar.gz";
-    break;
-  case "win32":
-  default:
-    console.error(`OS not supported`);
-    process.exit(1);
+let platformName;
+if (process.platform === "win32" || GOOS === "windows") {
+  fileName = "release_windows.zip";
+  platformName = "Windows";
+} else if (process.platform === "darwin" || GOOS === "darwin") {
+  // Use this code when  simulator will be available for MacOs arm64
+  // if (process.arch === "arm64" || GOARCH === "arm64") {
+  //   fileName = "release_macos-arm64.tar.gz";
+  //   platformName = "MacOs arm64";
+  // } else {
+  fileName = "release_macos.tar.gz";
+  platformName = "MacOs amd64";
+  // }
+} else {
+  platformName = "Linux";
+  fileName = "release_linux.tar.gz";
 }
 
 const url = `${TESTER_RELEASE_URL}/${VERSION}/${fileName}`;
@@ -27,7 +35,16 @@ const url = `${TESTER_RELEASE_URL}/${VERSION}/${fileName}`;
 let outputDir = path.join(process.cwd(), "simulator");
 fs.mkdirSync(outputDir, { recursive: true });
 
-const cmd = `wget -qO- ${url} | tar xz -C ${outputDir} --strip-components 1`;
+console.log(`Downloading simulator binary for ${platformName} at url: ${url}`);
+child_process.execSync(`curl -Lo ${fileName} ${url}`);
+
+let cmd;
+if (platformName === "Windows") {
+  cmd = `unzip -j ${fileName}  "massa/massa-sc-tester.exe" -d ${outputDir}`;
+} else {
+  cmd = `tar xf ${fileName} -C ${outputDir} --strip-components 1`;
+}
+console.log(`Extracting simulator binary in ${outputDir}`);
 child_process.execSync(cmd);
 
 process.exit(0);
